4a336bd38573b651a7c79192a1f8efe582cc22c7
Sourabh Pathak <63462210+sourabh1428@users.noreply.github.com>
2023-10-21 20:44:09 +0530

    Delete zocket-dashboard directory

--- zocket-dashboard/src/Components/form1.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Form1() {
-  return (
-    <div><h1>form1</h1></div>
-  )
-}
--- zocket-dashboard/src/Components/form2.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Form2() {
-  return (
-    <div><h1>Form</h1></div>
-  )
-}
